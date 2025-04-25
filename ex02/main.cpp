cfc1756804487ba7611462f26a868e30b01f3907
Alex Mylle <alexm@live.be>
2025-04-25 15:36:19 +0200

    updated the testing of ex02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -10,33 +10,65 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "MutantStack.hpp"
 #include <iostream>
-int main()
+#include "MutantStack.hpp"
+#include <list>
+int	main()
 {
-    MutantStack<int> stack;
+std::cout << "MutantStack test: \n";
+MutantStack<int>	mstack;
 
-    // Push some elements onto the stack
-    stack.push(10);
-    stack.push(20);
-    stack.push(30);
-    stack.push(40);
+mstack.push(5);
+mstack.push(17);
 
-    // Print elements using the forward iterator (begin and end)
-    std::cout << "Forward iteration: ";
-    for (MutantStack<int>::iterator it = stack.begin(); it != stack.end(); ++it)
-    {
-        std::cout << *it << " ";
-    }
-    std::cout << std::endl;
+std::cout << mstack.top() << std::endl;
 
-    // Print elements using the reverse iterator (rbegin and rend)
-    std::cout << "Reverse iteration: ";
-    for (MutantStack<int>::reverse_iterator it = stack.rbegin(); it != stack.rend(); ++it)
-    {
-        std::cout << *it << " ";
-    }
-    std::cout << std::endl;
+mstack.pop();
 
-    return 0;
+std::cout << mstack.size() << std::endl;
+
+mstack.push(3);
+mstack.push(5);
+mstack.push(737);
+mstack.push(0);
+
+MutantStack<int>::iterator it = mstack.begin();
+MutantStack<int>::iterator ite = mstack.end();
+
+++it;
+--it;
+while (it != ite)
+{
+	std::cout << *it << std::endl;
+	++it;
+}
+std::stack<int> s(mstack);
+
+std::cout << "List test: \n";
+std::list<int>	list;
+
+list.push_back(5);
+list.push_back(17);
+
+std::cout << list.back() << std::endl;
+
+list.pop_back();
+
+std::cout << list.size() << std::endl;
+
+list.push_back(3);
+list.push_back(5);
+list.push_back(737);
+list.push_back(0);
+
+std::list<int>::iterator list_it = list.begin();
+std::list<int>::iterator list_ite = list.end();
+
+++list_it;
+--list_it;
+while (list_it != list_ite)
+{
+	std::cout << *list_it << std::endl;
+	++list_it;
+}
 }
